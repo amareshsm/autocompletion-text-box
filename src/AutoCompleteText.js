e8890ee92d53e0fbeb943b7f4dda23c193f42c76
amareshsm <amareshsm13@gmail.com>
2020-08-10 01:02:25 +0530

    updated ui

--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import './AutoCompleteText.css';
+ import parse from 'html-react-parser';
+
 
 export default class  AutoCompleteText extends React.Component{
     constructor(props){
         super(props);
-        this.items=['Telegram','Tesla','Thoshiba','facebook','google','chargebee','Twillio',
-        'slack','freshworks','zoho','tcs','cts','Tech Mahindra','tvs','torrent'];  
+        this.items=['Telegram','Tesla','Thoshiba','facebook','amazon','amazon console','google','chargebee','Twillio',
+        'slack','freshworks','zoho','tcs','cts','Tech Mahindra','tvs','torrent','amazon sales'];  
         this.state={suggestions:[],inputText:''}
 }
 
 onChange(e){
     const value=e.target.value;
     let suggestions=[];
-    if (value.length >0 ){
-        const regex= new RegExp(`^${value}`,'i');
+    if (value.trim().length > 0 ){
+        const regex= new RegExp(`^${value.trim()}`,'i');
         suggestions=this.items.sort().filter(v => regex.test(v))
+        suggestions.map((val,index)=>{
+            let endIndex=value.length;
+            suggestions[index]=`<span class='highlight'>${value}</span>`+val.slice(endIndex)
+        })
     }
     this.setState(()=>({suggestions,inputText:value}));
 }
 
 suggestionSelected(value){
+    let val=value.replace(`<span class='highlight'>`,'').replace(`</span>`,'')
     this.setState(()=>({
-       inputText:value,
+       inputText:val,
        suggestions:[]
     }))
 }
@@ -31,7 +38,7 @@ renderSuggestions(){
     if(suggestions.length===0){
         return null;
     }
-    return (<ul> {suggestions.map((item)=> <li key={item} onClick={()=>this.suggestionSelected(item)}>{item}</li>)}</ul>)
+    return (<ul> {suggestions.map((item)=> <li key={item} onClick={()=>this.suggestionSelected(item)}>{parse(item)}</li>)}</ul>)
 }
 
 
